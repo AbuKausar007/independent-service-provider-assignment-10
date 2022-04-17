3359c86c6f21c0a2a63c772424881badd72e8817
AbuKausar007 <a.25kausar@gmail.com>
2022-04-17 13:23:51 +0600

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/LandingPage/Home/Home";
 import Services from "./Pages/LandingPage/Services/Services";
 import Header from "./Pages/Shared/Header/Header";
 import Footer from "./Pages/Shared/Footer/Footer";
+import About from "./Pages/About/About";
+import Blogs from "./Pages/Blogs/Blogs";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
+import NotFound from "./Pages/NotFound/NotFound";
 
 function App() {
   return (
@@ -12,7 +16,12 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/About" element={<About></About>}></Route>
         <Route path="/services" element={<Services></Services>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,11 +1,59 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import logo from "../../../images/logo.png";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <h1>it's header</h1>
-    </div>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand as={Link} to="/" href="#home">
+          <img src={logo} height={50} alt="" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/" href="#home">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about" href="#about">
+              About
+            </Nav.Link>
+            <Nav.Link as={Link} to="/services" href="#services">
+              Services
+            </Nav.Link>
+            <Nav.Link as={Link} to="/blogs" href="#blogs">
+              Blogs
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link as={Link} to="/login" href="#login">
+              Login
+            </Nav.Link>
+            <Nav.Link>SomeThing</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
 export default Header;
+
+// <nav className="navbar">
+//   <header className="container py-3 d-flex justify-content-between">
+//     <div>
+//       <Link to="/">Logo</Link>
+//     </div>
+//     <div className="middle-link w-50 d-flex justify-content-evenly ">
+//       <Link to="/">Home</Link>
+//       <Link to="/about">About</Link>
+//       <Link to="/services">Services</Link>
+//       <Link to="/blogs">Blogs</Link>
+//     </div>
+//     <div>
+//       <Link to="/login">Login</Link>
+//     </div>
+//   </header>
+// </nav>
